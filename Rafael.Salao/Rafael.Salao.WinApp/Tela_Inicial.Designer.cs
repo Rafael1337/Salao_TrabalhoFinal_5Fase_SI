64bd85df9b5363327892af91815190acf09ae98c
rafael.aldo <rafael.aldo@nddigital.com.br>
2019-05-24 16:32:07 -0300

    .

--- Rafael.Salao/Rafael.Salao.WinApp/Tela_Inicial.Designer.cs
+++ Rafael.Salao/Rafael.Salao.WinApp/Tela_Inicial.Designer.cs
@@ -69,7 +69,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(634, 450);
+            this.ClientSize = new System.Drawing.Size(634, 341);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Tela_Inicial";
