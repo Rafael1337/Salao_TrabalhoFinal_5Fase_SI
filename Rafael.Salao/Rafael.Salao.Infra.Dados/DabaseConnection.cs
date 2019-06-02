8e09fa766b5d487a209353e8c2976647386cd008
Rafael Aldo <rafael_aldo_s@hotmail.com>
2019-06-02 15:15:46 -0300

    removido entity framework criado classe para conexao com banco e também adicionado arquivo para gravar conectionstring.

--- Rafael.Salao/Rafael.Salao.Infra.Dados/DabaseConnection.cs
+++ Rafael.Salao/Rafael.Salao.Infra.Dados/DabaseConnection.cs
@@ -1,17 +1,65 @@
 ﻿
 
+using System.Data.SqlClient;
+using System.IO;
+using System.Xml.Linq;
+using Rafael.Salao.Infra.Dados.Helper;
 namespace Rafael.Salao.Infra.Dados
 {
     public class DabaseConnection
     {
-        public void VerifyConnectionString(string server, string _database, string _user, string _password)
-        {
+        public static SqlConnection connection_created;
+        public static string connection_string = "";
 
+        public static void CreateConnection()
+        {
+            GetConnectionString();
+            connection_created = new SqlConnection(connection_string);
+            try
+            {
+                connection_created.Open();
+                CreateTable();
+                connection_created.Close();
+            }
+            catch (SqlException)
+            {
+                CreateConnection();
+            }
         }
-        public void InitializeConnection()
-        {
 
-            DataContext DC = new DataContext();
+        private static void GetConnectionString()
+        {
+            if (File.Exists(@"databaseconnection.xml"))
+            {
+                CriaNovoArquivoDeConexao();
+            }
+            else
+            {
+                try
+                {
+                    CreateDatabase();
+                }
+                catch (SqlException) { }
+                NewConnectionWindow NCW = new NewConnectionWindow(); //Chamar nova tela para conexao de banco
+                NCW.ShowDialog();
+            }
+        }
+
+        private static void CriaNovoArquivoDeConexao()
+        {
+            XDocument xdoc = new XDocument();
+            xdoc = XDocument.Load(@"databaseconnection.xml");
+            connection_string = @"Data Source=" + xdoc.FindElement("server").Value + ";Initial Catalog=" + xdoc.FindElement("database").Value + ";User ID=" + xdoc.FindElement("user").Value + ";Password=" + xdoc.FindElement("password").Value + ";";
+        }
+
+        private static void CreateDatabase()
+        {
+            SqlConnection myConn = new SqlConnection("Server=localhost;Integrated security=SSPI;database=master");
+
+            SqlCommand sqlexecute = new SqlCommand("CREATE DATABASE SALAO_DATABASE;", myConn);
+            myConn.Open();
+            sqlexecute.ExecuteNonQuery();
+            myConn.Close();
         }
     }
 }
---
+++ Rafael.Salao/Rafael.Salao.Infra.Dados/Helper/XmlHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Rafael.Salao.Infra.Dados.Helper
+{
+    public static class XmlHelper
+    {
+        public static XElement FindElement(this XDocument document, string elementName)
+        {
+            return document.Descendants(document.Root.Name.Namespace + elementName).FirstOrDefault();
+        }
+    }
+}
