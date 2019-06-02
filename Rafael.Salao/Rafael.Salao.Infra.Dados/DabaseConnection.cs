aa33e6eb52d0f13e3fa6221431f7f8886e6ad80e
Rafael Aldo <rafael_aldo_s@hotmail.com>
2019-06-02 00:12:51 -0300

    Tentando mexer com essa joça de entity

--- Rafael.Salao/Rafael.Salao.Infra.Dados/DabaseConnection.cs
+++ Rafael.Salao/Rafael.Salao.Infra.Dados/DabaseConnection.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Rafael.Salao.Infra.Dados
 {
     public class DabaseConnection
     {
+        public void VerifyConnectionString(string server, string _database, string _user, string _password)
+        {
+
+        }
+        public void InitializeConnection()
+        {
+
+            DataContext DC = new DataContext();
+        }
     }
 }
---
+++ Rafael.Salao/Rafael.Salao.Infra.Dados/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using Rafael.Salao.Dominio;
+
+namespace Rafael.Salao.Infra.Dados
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("SALAO_DATABASE")
+        {
+            if (!Database.Exists("SALAO_DATABASE"))
+                Database.SetInitializer(new DropCreateDatabaseAlways<DataContext>());
+        }
+        public DbSet<Agenda> _agenda { get; set; }
+    }
+}
