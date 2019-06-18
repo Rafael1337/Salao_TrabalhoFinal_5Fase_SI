548c3cc5bdda62e2d92ee032fd5dd26c40fd1c44
Rafael Aldo <rafael_aldo_s@hotmail.com>
2019-06-18 07:44:00 -0300

    criado metodo GetConnectionForAppConfig e Construct AppConfig

--- Rafael.Salao/Rafael.Salao.Infra.Dados/Helper/AppConfigHelper.cs
+++ Rafael.Salao/Rafael.Salao.Infra.Dados/Helper/AppConfigHelper.cs
@@ -6,11 +6,14 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Rafael.Salao.Infra.Dados.Helper
 {
     public class AppConfigHelper
     {
+        string first_folder;
+        string connectionStringApp;
         public void CreateAppSettings()
         {
             StringBuilder sb = new StringBuilder();
@@ -22,8 +25,8 @@ namespace Rafael.Salao.Infra.Dados.Helper
             sb.AppendLine("<add key = 'ConnectionStringName' value = 'SqlServer_Teste' />");
             sb.AppendLine("</appSettings>");
             sb.AppendLine("<connectionStrings>");
-            sb.AppendLine("<add name = 'SqlServer_Teste' connectionString = 'Server=localhost;Database=PizzariaDB_Teste;User Id=sa; Password=Password_123'  providerName = 'System.Data.SqlClient' />");
-            sb.AppendLine("<add name = 'MySql_Teste' connectionString = 'Server=localhost;Database=pizzariadb_teste;Uid=root;Pwd=P@ssw0rd;Connect Timeout=30;' providerName = 'MySql.Data.MySqlClient' />");
+            sb.AppendLine("<add name = 'SqlServer_Teste' connectionString='Server=localhost;Database=PizzariaDB_Teste;User Id=sa; Password=Password_123'  providerName = 'System.Data.SqlClient' />");
+            sb.AppendLine("<add name = 'MySql_Teste' connectionString='Server=localhost;Database=pizzariadb_teste;Uid=root;Pwd=P@ssw0rd;Connect Timeout=30;' providerName = 'MySql.Data.MySqlClient' />");
             sb.AppendLine("</connectionStrings>");
             sb.AppendLine("</configuration>");
             File.WriteAllText("App.config", sb.ToString());
@@ -33,8 +36,25 @@ namespace Rafael.Salao.Infra.Dados.Helper
         {
             string first = Directory.GetParent(Directory.GetCurrentDirectory()).ToString();
             string second = Directory.GetParent(first).ToString();
-            string last = Directory.GetParent(second).ToString();
-            File.Copy("App.config", String.Concat(last+ "\\Rafael.Salao.Infra.Dados.Testes\\", "App.config"));
+            first_folder = Directory.GetParent(second).ToString();
+            File.Copy("App.config", String.Concat(first_folder + "\\Rafael.Salao.Infra.Dados.Testes\\", "App.config"));
+        }
+
+        public void GetConnectionForAppConfig()
+        {
+            XDocument xdoc = new XDocument();
+            xdoc = XDocument.Load(first_folder + "\\Rafael.Salao.WinApp\\bin\\Debug\\config");
+            string servidor = xdoc.FindElement("servidor").Value;
+            string database = xdoc.FindElement("banco").Value;
+            string usuario = xdoc.FindElement("usuario").Value;
+            string senha = xdoc.FindElement("senha").Value;
+
+            connectionStringApp = "Server="+ servidor + ";Database="+ database + ";User Id="+ usuario + ";Password="+ senha + "";
+        }
+        public void ConstructAppConfig()
+        {
+            XDocument appdoc = new XDocument();
+            XDocument.Load(first_folder + "\\Rafael.Salao.Infra.Dados.Testes\\App.config");
         }
     }
 }
