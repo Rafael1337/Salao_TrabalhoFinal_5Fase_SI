d80f946e19f7ef96558206b2c2bacfc4ea8c8f74
rafael.aldo <rafael.aldo@nddigital.com.br>
2019-06-25 16:10:17 -0300

    criado metodo FirstTimeOpenInitCaixa

--- Rafael.Salao/Rafael.Salao.Infra.Dados/Caixa/CaixaDao.cs
+++ Rafael.Salao/Rafael.Salao.Infra.Dados/Caixa/CaixaDao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,16 @@ namespace Rafael.Salao.Infra.Dados.Caixa
     public class CaixaDao
     {
         private static double saldo_adicionado;
+
+        private static string init_caixa = "INSERT INTO TBCAIXA(SALDO, DATA_ATUAL) VALUES(0, CONVERT(date, SYSDATETIME()))";
         private static string _scriptInsercao = @"UPDATE TBCAIXA SET SALDO = (SELECT SALDO FROM TBCAIXA WHERE DATA_ATUAL = CONVERT (date, SYSDATETIME())) + " + saldo_adicionado;
 
-        public void FirstTimeOpenInitCaixa()
+        public void FirstTimeOpenInitCaixa(SqlConnection connection)
         {
-            string init_caixa = "INSERT INTO TBCAIXA(SALDO, DATA_ATUAL) VALUES(0, CONVERT(date, SYSDATETIME()))";
+            SqlCommand cmd = new SqlCommand(init_caixa, connection);
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            connection.Close();
         }
-
     }
 }
