a203cb7ef1c5417517fc2838b8888cc66b4f81d4
rafael.aldo <rafael.aldo@nddigital.com.br>
2019-06-25 18:42:17 -0300

    Montando rotina de inicialização de caixa.

--- Rafael.Salao/Rafael.Salao.Infra.Dados/Caixa/CaixaDao.cs
+++ Rafael.Salao/Rafael.Salao.Infra.Dados/Caixa/CaixaDao.cs
@@ -11,14 +11,22 @@ namespace Rafael.Salao.Infra.Dados.Caixa
     {
         private static double saldo_adicionado;
 
-        private static string init_caixa = "INSERT INTO TBCAIXA(SALDO, DATA_ATUAL) VALUES(0, CONVERT(date, SYSDATETIME()))";
+        private static string init_caixa = @"INSERT INTO TBCAIXA(SALDO, DATA_ATUAL) VALUES(0, CONVERT(date, SYSDATETIME()))";
         private static string _scriptInsercao = @"UPDATE TBCAIXA SET SALDO = (SELECT SALDO FROM TBCAIXA WHERE DATA_ATUAL = CONVERT (date, SYSDATETIME())) + " + saldo_adicionado;
-
+        private static string _get_register = @"SELECT TOP(1)* FROM TBCAIXA";
         public void FirstTimeOpenInitCaixa(SqlConnection connection)
         {
+            VerifyContainsData(connection);
             SqlCommand cmd = new SqlCommand(init_caixa, connection);
             connection.Open();
             cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+        private void VerifyContainsData(SqlConnection connection)
+        {
+            SqlCommand cmd = new SqlCommand(_get_register, connection);
+            connection.Open();
+            var retorno = (Int32)cmd.ExecuteScalar();
             connection.Close();
         }
     }
